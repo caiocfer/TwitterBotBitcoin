f9f30a32c8b51f9369d5be67c4b016091ef3b99f
Caio <caiocfer@gmail.com>
2020-09-02 01:55:35 -0300

    Added User interface

--- Coinmarket.py
+++ Coinmarket.py
@@ -1,25 +1,51 @@
+import tkinter as tk
+import tweepy
 import threading
 from pycoingecko import CoinGeckoAPI
-import tweepy
 
-consumer_key = ''
-consumer_secret = ''
+win = tk.Tk()
 
-access_token = ''
-access_token_secret = ''
+consumer_key_label = tk.Label(win, text="consumer_key")
+consumer_key = tk.Entry(win)
+consumer_key_label.grid(row=0)
+consumer_key.grid(row=0,column=1)
 
+consumer_secret_label = tk.Label(win, text="consumer_secret")
+consumer_secret = tk.Entry(win)
+consumer_secret_label.grid(row=1)
+consumer_secret.grid(row=1,column=1)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+access_token_label = tk.Label(win, text="access_token ")
+access_token = tk.Entry(win)
+access_token_label.grid(row=2)
+access_token.grid(row=2,column=1)
+
+access_token_secret_label = tk.Label(win, text="access_token_secret")
+access_token_secret = tk.Entry(win)
+access_token_secret_label.grid(row=3)
+access_token_secret.grid(row=3,column=1)
 
 
 cg = CoinGeckoAPI()
 espera = 60
 bitcoin_antes = cg.get_price(ids='bitcoin', vs_currencies='BRL')
 
-def update_bitcoin():
-    threading.Timer(espera, update_bitcoin).start()
+def startbot():
+    cg = CoinGeckoAPI()
+    espera = 60
+    bitcoin_antes = cg.get_price(ids='bitcoin', vs_currencies='BRL')
+    auth = tweepy.OAuthHandler(consumer_key.get(), consumer_secret.get())
+    auth.set_access_token(access_token.get(), access_token_secret.get())
+    api = tweepy.API(auth)
+    update_coin()
+
+
+
+def update_coin():
+    auth = tweepy.OAuthHandler(consumer_key.get(), consumer_secret.get())
+    auth.set_access_token(access_token.get(), access_token_secret.get())
+    api = tweepy.API(auth)
+    threading.Timer(espera, update_coin).start()
     global bitcoin_antes
     cg = CoinGeckoAPI()
 
@@ -40,10 +66,7 @@ def update_bitcoin():
     else:
         bitcoin_agora = cg.get_price(ids='bitcoin', vs_currencies='BRL')
 
+button_start = tk.Button(win, text="Calculate", command=startbot())
+button_start.grid(row=4,column=1)
 
-
-update_bitcoin()
-
-
-
-
+win.mainloop()
